5b474afd7891bd7e0ce032fb695399f7526a3602
BednarikK23 <bednarik2387@gmail.com>
2023-08-27 22:52:04 +0200

    updatet slices about assigments

--- goLang/slices/slices.go
+++ goLang/slices/slices.go
@@ -31,17 +31,18 @@ func main() {
 	slice := primes[1:4]
 	stringIt(slice)
 	/*
-		SLICE CREATION
-		- how to make slice without creating array explicitly (it will create array under the hood for us):
+			SLICE CREATION
+			- how to make slice without creating array explicitly (it will create array under the hood for us):
+			- Slices created with make will be filled with the zero value of the type.
 		make takes:
-			- types of elems
-			- current length
-			- capcity to what slice can grow..., this argument is optional, when not set capacity = length
-				- if cap exceeds the slice is reallocated...
-		- functions:
-			len - returns current length of slice/array
-			cap - returns allocated capacity of the array
-			cap, len - when slice IS NIL RETURNS 0
+				- types of elems
+				- current length
+				- capcity to what slice can grow..., this argument is optional, when not set capacity = length
+					- if cap exceeds the slice is reallocated...
+			- functions:
+				len - returns current length of slice/array
+				cap - returns allocated capacity of the array
+				cap, len - when slice IS NIL RETURNS 0
 	*/
 	costs := make([]float64, 5, 10) // type, length, capacity
 
@@ -95,6 +96,36 @@ func main() {
 		fmt.Println(i, fruit)
 	}
 
+	/// assigment
+	test([]string{
+		"Welcome to the movies!",
+		"Enjoy your popcorn!",
+		"Please don't talk during the movie!",
+	})
+	test([]string{
+		"I don't want to be here anymore",
+		"Can we go home?",
+		"I'm hungry",
+		"I'm bored",
+	})
+	test([]string{
+		"Hello",
+		"Hi",
+		"Hey",
+		"Hi there",
+		"Hey there",
+		"Hi there",
+		"Hello there",
+		"Hey there",
+		"Hello there",
+		"General Kenobi",
+	})
+
+	// assigment 2
+	sum_main()
+
+	// assigment 3
+	cost_main()
 }
 
 func createMatrix(rows, cols int) [][]int {
@@ -143,4 +174,107 @@ func stringIt(nums []int) {
 	for i := 0; i < len(nums); i++ {
 		println(nums[i])
 	}
+}
+
+func getMessageCosts(messages []string) []float64 {
+	costs := make([]float64, len(messages))
+	for i := 0; i < len(messages); i++ {
+		message := messages[i]
+		cost := float64(len(message)) * 0.01
+		costs[i] = cost
+	}
+	return costs
+}
+
+// don't edit below this line
+
+func test(messages []string) {
+	costs := getMessageCosts(messages)
+	fmt.Println("Messages:")
+	for i := 0; i < len(messages); i++ {
+		fmt.Printf(" - %v\n", messages[i])
+	}
+	fmt.Println("Costs:")
+	for i := 0; i < len(costs); i++ {
+		fmt.Printf(" - %.2f\n", costs[i])
+	}
+	fmt.Println("===== END REPORT =====")
+}
+
+func sum(nums ...float64) float64 {
+	total := 0.0
+	for i := 0; i < len(nums); i++ {
+		total += nums[i]
+	}
+	return total
+}
+
+// don't edit below this line
+
+func test2(nums ...float64) {
+	total := sum(nums...)
+	fmt.Printf("Summing %v costs...\n", len(nums))
+	fmt.Printf("Bill for the month: %.2f\n", total)
+	fmt.Println("===== END REPORT =====")
+}
+
+func sum_main() {
+	test2(1.0, 2.0, 3.0)
+	test2(1.0, 2.0, 3.0, 4.0, 5.0)
+	test2(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0)
+	test2(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0)
+}
+
+type cost struct {
+	day   int
+	value float64
+}
+
+func getCostsByDay(costs []cost) []float64 {
+	costByDay := []float64{}
+	for i := 0; i < len(costs); i++ {
+		cost := costs[i]
+		for cost.day >= len(costByDay) {
+			costByDay = append(costByDay, 0.0)
+		}
+		costByDay[cost.day] += cost.value
+	}
+	return costByDay
+}
+
+// dont edit below this line
+
+func cost_test(costs []cost) {
+	fmt.Printf("Creating daily buckets for %v costs...\n", len(costs))
+	costsByDay := getCostsByDay(costs)
+	fmt.Println("Costs by day:")
+	for i := 0; i < len(costsByDay); i++ {
+		fmt.Printf(" - Day %v: %.2f\n", i, costsByDay[i])
+	}
+	fmt.Println("===== END REPORT =====")
+}
+
+func cost_main() {
+	cost_test([]cost{
+		{0, 1.0},
+		{1, 2.0},
+		{1, 3.1},
+		{2, 2.5},
+		{3, 3.6},
+		{3, 2.7},
+		{4, 3.34},
+	})
+	cost_test([]cost{
+		{0, 1.0},
+		{10, 2.0},
+		{3, 3.1},
+		{2, 2.5},
+		{1, 3.6},
+		{2, 2.7},
+		{4, 56.34},
+		{13, 2.34},
+		{28, 1.34},
+		{25, 2.34},
+		{30, 4.34},
+	})
 }
